705934d4c6f3f834110c5803fb3b3b2b89799dab
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-19 17:34:27 -0400

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,10 @@
+using system;
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
