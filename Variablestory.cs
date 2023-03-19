5f0e68d67b4e8773afb2f60560418d772d166212
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-19 18:43:12 -0400

    Create Variablestory.cs

---
+++ Variablestory.cs
@@ -0,0 +1,27 @@
+using system;
+namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            console.readline(s);
+           console.readline(age);
+            console.readline(dd);
+            console.readline(ss);
+            string s;
+            string ss;
+            int age;
+            string place;
+ 
+            Consule.writeline("Please enter your name,age,a decimal and your best friend's name");
+            s= console.readline();
+           age=console.readline();
+            place=console.readline();
+           ss= console.readline();
+            
+            Consule.writeline(s+" age" + age + "met "+ss+"at the "+place);
+           
+        }
+    }
+}
