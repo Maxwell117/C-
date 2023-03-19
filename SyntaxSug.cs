acf86314029e0946e3db799cac0246b89e2db40a
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-19 19:29:20 -0400

    Create SyntaxSug.cs

---
+++ SyntaxSug.cs
@@ -0,0 +1,15 @@
+using system;
+
+namespace SyntaxSug
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+         var answer = 4;
+        var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+        Console.WriteLine(response);
+        }
+    }
+}
