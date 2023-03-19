ec4518894c8a01d92df232bf561ea95ece5992ad
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-19 19:10:55 -0400

    Create operators.cs

---
+++ operators.cs
@@ -0,0 +1,27 @@
+using system;
+using math:
+namespace MethodExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           var a=27;
+           var b=4;
+           var quot=a/b;
+           var rem=a%b;
+            double rad=77;
+           double area;
+           console.writeline(a"/ "+b+"= " quot);
+           console.writeline(a"% "+b+"= " rem);
+        area=  areaofacircle(rad);
+          
+
+        }
+
+         public static double areaofacircle(double radios)
+           {
+           return Math.PI * Math.Pow(radius, 2);
+           }
+    }
+}
