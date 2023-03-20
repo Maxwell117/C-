b783ca7f0636d6db9dd69071a5a3f7c6c5b7629d
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-20 02:12:38 -0400

    Create unittest2.cs

---
+++ unittest2.cs
@@ -0,0 +1,65 @@
+using System;
+namespace UnitTestingExercise
+{
+    public class Calculator
+    {
+        // Step 1: Add a reference to your UnitTestingExercise project:
+
+        // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
+        // and add your reference to the UnitTestingExercise project
+
+
+
+        // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer
+        //For Example:
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+
+        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
+
+
+        // Step 4: 
+        // Create a Subtract method that accepts 2 integers
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int sub(int num1, int num2)
+        {   if(num1>num2)
+            return num1-num2;
+            else if(num2>num1)
+            return num2-num1;
+            else
+            return num1-num2;
+        }
+
+        // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
+
+
+        // Step 6: 
+        // Create a Multiply method that passes 2 integers
+        public int mult(int num1, int num2)
+        {
+            return num1 * num2; 
+        }
+
+
+        // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
+
+
+        // Step 8: 
+        // Create a Divide method that passes 2 integers
+         public int div(int num1, int num2)
+        {   if(num1>num2)
+            return num1/num2;
+            else if(num2>num1)
+            return num2/num1;
+            else
+            return 1;
+        }
+        // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
+
+
+        //*****NOW RUN ALL OF YOUR TESTS*********//
+    }
+}
