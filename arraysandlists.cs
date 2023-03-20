353e29ce9a3f454593c8069efe75219d43c333d1
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-19 20:18:22 -0400

    Create arraysandlists.cs

---
+++ arraysandlists.cs
@@ -0,0 +1,27 @@
+using system;
+using System.Collections.Generic;
+namespace arraysandlists
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        list<int> even=new list<int>();
+        list<int> odd=new list<int>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%2==0)
+            {
+            even.add(nums[i]);
+            }
+            else
+            {
+            odd.add(nums[i]);
+            }
+        }
+
+    }
+}
+
