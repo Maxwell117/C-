67f78977b8a706d86fed6de7fafc90435ff49ee3
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-21 05:22:31 -0400

    Create app.js

---
+++ SQL/app.js
@@ -0,0 +1,36 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstname="Rosie";
+let lastName="Cotton";
+var age=51;
+// Exercise 2
+lastname="Gamgee";
+age=52;
+// Exercise 3
+let language="JavaScript";
+let createdyear=1995;
+let isCaseSensitive=true;
+
+console.log(language + "was created in" + createdyear);
+
+let product="t-shirt"
+let price=19.99;
+let isonsale=false;
+let salepercentage=15;
+let stock=0;
+let instock=false;
+let selectedsize='M';
+
+console.log(product + " is $" + price + " with " + stock + " in stock");
+
+let title="Name of the Wind";
+let author="Patrick Rothfuss";
+let pageCount=722;
+let bookmark=456;
+let hadRead=true;
+
+console.log(title + " is " + pageCount + " pages long, your bookmark was on page " + bookmark);
