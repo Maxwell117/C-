0cb37498050b40c56d2defd69218570b89a901ef
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 18:55:02 -0400

    Create Bird.cs

---
+++ inherit/Bird.cs
@@ -0,0 +1,18 @@
+namespace inheritance
+{
+class Bird : Animal
+{
+   private int wings{get;set;}
+   private bool flight{get;set;}
+   private int flightspeed{get;set;}
+   private int eggs{get;set;}
+
+   public Bird
+   {
+   legs=2;
+   skin="feathers";
+   weight=2;
+   diet="omnivore";
+   }
+}
+}
