f4f8a8323f376c3ad85b7c42329fa1424888a05e
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 03:22:18 -0400

    Update selectionstatements.cs

--- selectionstatements.cs
+++ selectionstatements.cs
@@ -1,28 +1,51 @@
-using system;
-using random;
+using System;
+
 namespace MethodExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-
-         int slotmachine=new random();
+         var n = int.Parse(Console.ReadLine());
+         var slotmachine= new Random();
          int rand=slotmachine.Next(1,100);
-         int n=5;
          if(rand>n)
          {
-         Console.writeline("Too high");
+         Console.WriteLine("Too high");
          }
          else if(rand<n)
          {
-         Console.writeline("Too low");
+         Console.WriteLine("Too low");
          }
          else
          {
-         Console.writeline("You found it!");
+         Console.WriteLine("You found it!");
          }
 
-        }
-    }
+		 Console.WriteLine("What is your favorite subject in school?");
+	     var sub=Console.ReadLine();
+		
+	     switch(sub)
+		 {
+			 case "math":
+				 Console.WriteLine("torture");
+				 break;
+		     case "english":
+				 Console.WriteLine("drama");
+				  break;
+		     case "chemistry":
+				 Console.WriteLine("breaking bad");
+				  break;
+			 case "art":
+				 Console.WriteLine("fun");
+				  break;
+	         case "history":
+				 Console.WriteLine("boring");
+				  break;
+			 default:
+				  Console.WriteLine("Never heard of it");
+		          break;
+          }
+		}
+	}
 }
