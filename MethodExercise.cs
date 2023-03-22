540168c2736daccd8ee8e59ea478ad5c7ca8f53a
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 15:32:40 -0400

    Update MethodExercise.cs

--- MethodExercise.cs
+++ MethodExercise.cs
@@ -1,20 +1,8 @@
-using system;
+using System;
 namespace MethodExercise
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-           
- 
-            Console.writeline("please enter 2 numbers");
-            string a=console.readline();
-            string b=console.readline();
-            int reala=convert.Toint32(a);
-            int realb=convert.Toint32(b);
-            int result= add(a,b);
-            int result2= mult(a,b);
-             
             public static int add(int a,int b)
             {
             return a+b;
@@ -24,7 +12,41 @@ namespace MethodExercise
             {
             return a*b;
             }
-           
+            
+            public static int sub(int a,int b)
+            {
+            return a-b;
+            }
+            
+            public static double div(int a,int b)
+            {
+            return a/b;
+            }
+            
+            public static double mod(int a,int b)
+            {
+            return a%b;
+            }
+        
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("please enter 2 numbers");
+            var a=Convert.ToInt32(Console.ReadLine());
+            var b=Convert.ToInt32(Console.ReadLine());
+      
+            var result= add(a,b);
+            Console.WriteLine(result);
+            var result2= mult(a,b);
+              Console.WriteLine(result2);
+            var result3= sub(a,b);
+              Console.WriteLine(result3);
+            var result4= mod(a,b);
+               Console.WriteLine(result4);
+            var result5= div(a,b);
+               Console.WriteLine(result5);
+            
+             
         }
     }
 }
