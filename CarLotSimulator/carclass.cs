2ca11f7f43369c8b79889feba8f1bfbecd798916
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 18:12:47 -0400

    Create carclass.cs

---
+++ CarLotSimulator/carclass.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+namespace car
+{
+class Car 
+{
+ public int year{get;set;}; 									
+ public string model{get;set;};
+ public string make{get;set;};
+public bool IsDriveable{get;set;};
+ public string HonkNoise{get;set;};
+ public string EngineNoise{get;set;};
+     public Car()
+     {
+
+     }
+
+     public Car(int year,string model,string make,bool IsDriveable,string HonkNoise,string EngineNoise)
+     {
+     this.year=year;
+     this.model=model;
+     this.make=make;
+     this.IsDriveable=IsDriveable;
+     this.HonkNoise=HonkNoise;
+     this.EngineNoise=EngineNoise;
+     }
+
+public void MakeEngineNoise(string n)
+{
+Console.WriteLine(n);
+}
+
+
+public void MakeHonkNoise()
+{
+Console.WriteLine(HonkNoise);;
+}
+
+
+
+
+}
+}
