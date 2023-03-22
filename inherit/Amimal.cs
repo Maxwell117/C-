8d0a0c3cb978bda66559f3e6e395f95c861abd5d
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 18:53:30 -0400

    Create Amimal.cs

---
+++ inherit/Amimal.cs
@@ -0,0 +1,14 @@
+namespace inheritance
+{
+class Animal
+{
+   private int legs{get;set;}
+   private string skin{get;set;}
+   private int weight{get;set;}
+   private string diet{get;set;}
+
+   public Animal
+   {
+
+   }
+}
