f6d1787dc1941c5f2e53cc3676dd97fc0871da49
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 17:19:02 -0400

    Update Carclass.cs

---
+++ classcar/Carclass.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+namespace car
+{
+class Car 
+{
+ public int year{get;set;}; 									 public string model{get;set;};
+ public string make{get;set;};
+
+
+     public car()
+     {
+
+     }
+
+     public car(int year,string model,string make)
+     {
+     this.year=year;
+     this.model=model;
+     this.make=make;
+     }
+}
+}
