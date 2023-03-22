e68debb2bc442894e49e2c63a3bf62fc0a354db2
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 18:34:29 -0400

    Update and rename CarMain.cs to BankMain.cs

---
+++ bankaccount/BankMain.cs
@@ -0,0 +1,14 @@
+using System;
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 	 bankaccount bob=new bankaccount();
+      	   bob.balance=300;
+        
+               bob.deposit(500);
+        }
+    }
+}
