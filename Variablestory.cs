b377d335ac14121f02c4d16d91039a10d6c3cc9e
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 15:37:43 -0400

    Update Variablestory.cs

--- Variablestory.cs
+++ Variablestory.cs
@@ -1,26 +1,23 @@
-using system;
+using System;
 namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            console.readline(s);
-           console.readline(age);
-            console.readline(dd);
-            console.readline(ss);
+           
             string s;
             string ss;
             int age;
             string place;
  
-            Consule.writeline("Please enter your name,age,a decimal and your best friend's name");
-            s= console.readline();
-           age=console.readline();
-            place=console.readline();
-           ss= console.readline();
+            Console.WriteLine("Please enter your name,age,a decimal and your best friend's name");
+            s= Console.ReadLine();
+           age=Convert.ToInt32(Console.ReadLine());
+            place=Console.ReadLine();
+           ss= Console.ReadLine();
             
-            Consule.writeline(s+" age" + age + "met "+ss+"at the "+place);
+            Console.WriteLine(s+" age" + age + "met "+ss+"at the "+place);
            
         }
     }
