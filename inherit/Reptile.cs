600d41f8baabfa05803be8ab0b74374c076e9c4a
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 18:56:38 -0400

    Create Reptile.cs

---
+++ inherit/Reptile.cs
@@ -0,0 +1,19 @@
+using System;
+namespace inheritance
+{
+class Reptile : Animal
+{
+   private bool camoflague{get;set;}
+   private bool coldblooded{get;set;}
+   private string scalecolor{get;set;}
+   private bool venomous{get;set;}
+
+  public Reptile
+   {
+   legs=4;
+   skin="scales";
+   weight=4;
+   diet="herbivore";
+   }
+}
+}
