8c35f7d5653085b827348a2fb027384ba99a9b1e
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 01:30:55 -0400

    Add files via upload

---
+++ APIs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
