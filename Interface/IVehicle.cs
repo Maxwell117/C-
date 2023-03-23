64cef4f2790b20014e8eefbfae8a2a0ba6a7c650
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-23 00:48:27 -0400

    Add files via upload

---
+++ Interface/IVehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace interfaceexercise
+{
+    interface IVehicle
+    {
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        void Drive();
+        void DriveDefault()
+        {
+            Console.WriteLine("Default Drive inside interface");
+        }
+    }
+
+    interface ICompany
+    {
+        public string Logo { get; set; }
+        public DateTime DateEstablished { get; set; }
+    }
+}
---
+++ Interface/MotorCycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace interfaceexercise
+{
+    class MotorCycle : IVehicle, ICompany
+    {
+
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public bool HasSideCart { get; set; }
+        public string Logo { get; set; }
+        public DateTime DateEstablished { get; set; }
+
+
+
+
+
+
+
+        public void Drive()
+        {
+            Console.WriteLine("MotorCycle Drive");
+        }
+    }
+}
