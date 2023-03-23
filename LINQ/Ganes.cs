e6ca0e190d997afd180165e5ce2a3e7591eef11f
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-23 03:27:32 -0400

    Update Ganes.cs

--- LINQ/Ganes.cs
+++ LINQ/Ganes.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<string> games=new List<string>();
+games.Add("Doom 2");
+games.Add("Ocarina Of Time");
+games.Add("Pokemon Red");
+games.Add("Heretic");
+games.Add("phantasy star");
+games.Add("Quake");
+games.Add("Blood");
+games.Add("Super Mario 64");
+games.Add("Skyrim");
+
+var OrderedGames=games.OrderBy(name=>name.Length);
+
+foreach(var game in games)
+{
+Console.WriteLine(game);
+}
