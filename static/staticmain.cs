a4bd2f3909605629bba9b1df92458a979bcf453a
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-22 20:30:42 -0400

    Update and rename CarMain.cs to staticmain.cs

---
+++ static/staticmain.cs
@@ -0,0 +1,24 @@
+using System;
+namespace staticexercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+         var celc=TempConverter.FahrenheitToCelsius(70);
+         var fahr=TempConverter.CelsiusToFahrenheit(88);
+
+         Car honda=new car();
+          Console.WriteLine(CarLot.cnt);          
+         Car chey=new Car();
+          Console.WriteLine(CarLot.cnt);          
+	       Car troa=new Car()’;
+          Console.WriteLine(CarLot.cnt);          
+
+
+
+         
+        }
+     
+    }
+}
