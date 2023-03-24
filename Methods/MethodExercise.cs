26d837946a54723be29da738aa1a3e0e497f2798
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 00:44:44 -0400

    Add files via upload

---
+++ Methods/MethodExercise.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace MethodExercise
+{
+    public class Program
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static double div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static double mod(int a, int b)
+        {
+            return a % b;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("please enter 2 numbers");
+            var a = Convert.ToInt32(Console.ReadLine());
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var result = add(a, b);
+            Console.WriteLine(result);
+            var result2 = mult(a, b);
+            Console.WriteLine(result2);
+            var result3 = sub(a, b);
+            Console.WriteLine(result3);
+            var result4 = mod(a, b);
+            Console.WriteLine(result4);
+            var result5 = div(a, b);
+            Console.WriteLine(result5);
+
+
+        }
+    }
+}
---
+++ Methods/VariableStory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s;
+            string ss;
+            int age;
+            string place;
+
+            Console.WriteLine("Please enter your name,age,a decimal and your best friend's name");
+            s = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+            place = Console.ReadLine();
+            ss = Console.ReadLine();
+
+            Console.WriteLine(s + " age" + age + "met " + ss + "at the " + place);
+
+        }
+    }
+}
