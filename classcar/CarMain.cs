1fa7af40f4c388d04cf353892d6ee0a473f8b4f5
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 01:43:14 -0400

    Update CarMain.cs

--- classcar/CarMain.cs
+++ classcar/CarMain.cs
@@ -1,15 +1,15 @@
-namespace classes
+namespace car
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-        car honda=new car();
-        honda.year=2010;
-        honda.model="2A";
-        honda.make="x2x";
-       Console.writeline("year"+ honda.year + "model "+ honda.model + "make "+honda.make);
-        carlist<car>=new list<car>();
+            Car honda = new Car();
+            honda.year = 2010;
+            honda.model = "2A";
+            honda.make = "x2x";
+            Console.WriteLine("year" + honda.year + "model " + honda.model + "make " + honda.make);
+        List<Car> list = new List<Car>();
         }
     }
 }
