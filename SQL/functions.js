05f4059580d651e1d691eda1d712c223a392d477
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 03:27:39 -0400

    Update functions.js

--- SQL/functions.js
+++ SQL/functions.js
@@ -1,22 +1,22 @@
 //exercise 1
-Function printodds(n)
+function printodds(n)
 {
 	for(let i=0;i<n;i++)
 	{
  		if(i%2!=0)
 		{
-                       Console.Log(i+” is odd”’);
+        Console.Log(i+" is odd");
 		}
 	}
 }
 
 //exercise 2
-Function checkage(age,name)
+function checkage(age,name)
 {
 	if(age>16)
-           {
-	Console.Log("Congrats ${name}, you can drive!”);
-           }
+    {
+	Console.Log("Congrats ${name}, you can drive!");
+    }
 	else
 	{
 	Console.Log("Sorry ${userName}, but you need to wait until you're 16.");
@@ -24,85 +24,85 @@ Function checkage(age,name)
 }
 
 //exercise 3
-Function Quadmarker(x,y)
+function Quadmarker(x,y)
 {
 	if(x==0)
-           {
-	Console.Log((${x},${y}) is on the y axis”);
-           }
+    {
+	Console.Log(x+y+ "is on the y axis")
+    }
 	else if(y==0)
 	{
-	Console.Log((${x},${y}) is on the x axis”);
+	Console.Log(x+y+" is on the x axis")
 	}
-           else if(x>0&&y>0)
+    else if(x>0&&y>0)
+    {
+	Console.Log(x+y+" is on the 1 quadrant")
+    }
+    else if(x<0&&y<0)
 	{
-	Console.Log((${x},${y}) is on the 1 quadrant”);
+	Console.Log(x+y," is on the 4 quadrant")
 	}
-           else if(x<0&&y<0)
+    else if(x<0&&y>0)
 	{
-	Console.Log((${x},${y}) is on the 4 quadrant”);
+	Console.Log(x+y+" is on the 2 quadrant")
 	}
-            else if(x<0&&y>0)
+    else if(x>0&&y<0)
 	{
-	Console.Log((${x},${y}) is on the 2 quadrant”);
-	}
-            else if(x>0&&y<0)
-	{
-	Console.Log((${x},${y}) is on the 3 quadrant”);
+	Console.Log(x+y+ "is on the 3 quadrant")
 	}
 
 }
 
 //exercise 4
-Function TriangleClassifier(a,b,c)
+function TriangleClassifier(a,b,c)
 {
-      Var sum=a+b;
+      var sum=a+b;
       
-      Var ans=””;
+      var ans="";
       if(sum>c)
       {
-      ans=”Invalid Triangle”;
+      ans="Invalid Triangle";
       }
-     Else if(a==b&&a==c)
+     else if(a==b&&a==c)
      {
-     ans=”Equilateral Triangle”;
+     ans="Equilateral Triangle";
      }
-     Else if(a==b||a==c)
+     else if(a==b||a==c)
      {
-     ans=”Isosceles Triangle”;
+     ans="Isosceles Triangle";
      }
-     Else
+     else
      {
-      ans=”Scalene Triangle”;
+      ans="Scalene Triangle";
      }
 
      return ans;
 }
 
 //exercise 5
-Function Dataplan(planlimit,usage,day)
+function Dataplan(planlimit,usage,day)
 {
-     Const daysleft=30-day;
-     console.log(day+” days used ”+daysleft+ “ days left”);
+     const daysleft=30-day;
+     console.log(day+" days used "+daysleft+ " days left");
 
     var ave=planlimit/30;
-    var your_ ave=  usage/day;
-    Var rem=0; 
+    var your_ave=  usage/day;
+    var rem=0; 
     
-    console.log(“average daily use is ”+your_ave + “GB/Day”);
+    console.log("average daily use is "+your_ave + GB/Day);
    
     if(ave>your_ave)
     {
-     console.log(“You are UNDER the average usage ”+ave);
+     console.log("You are UNDER the average usage" +ave);
     } 
      if(ave<your_ave)
     {
      rem=ave-your_ave;;
-     console.log(“You are EXCEEDING the average usage ”+ave +”try to stay below ”+ rem);
+     console.log("You are EXCEEDING the average usage "+ave +" try to stay below "+ rem);
     }
    Else
    {
-   console.log(“You are on PAR with the average usage ”+ave);
+   console.log("You are on PAR with the average usage "+ave);
    }
 
      
