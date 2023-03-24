0bc9cd380ae9af3f6df63feb3fdb63925da1de14
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 02:28:31 -0400

    Update Motorcycle.cs

--- abstract/Motorcycle.cs
+++ abstract/Motorcycle.cs
@@ -1,18 +1,19 @@
+
 using System;
 namespace ConsoleUI
 {
-class Motorcycle : vehicle
-   {
-    public bool HasSideCart {get;set;} 
-    public override void driveabstract()
-   {
-   Console.WriteLine("motorcycle drive");
-   }
+    class Motorcycle : Vehicle
+    {
+        public bool HasSideCart { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("motorcycle drive");
+        }
 
-  public override void drivevirtual()
-   {
-   Console.WriteLine("motorcycle drive virtual");
-   }
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("motorcycle drive virtual");
+        }
 
-   }
+    }
 }
