3e16b3cdf2875c22aa89e1a533ec1c1c8eaa19e6
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 02:09:00 -0400

    Update Car.cs

--- static/Car.cs
+++ static/Car.cs
@@ -2,43 +2,43 @@ using System;
 using System.Collections.Generic;
 namespace car
 {
-class Car 
-{
- public int year{get;set;}; 									
- public string model{get;set;};
- public string make{get;set;};
-public bool IsDriveable{get;set;};
- public string HonkNoise{get;set;};
- public string EngineNoise{get;set;};
-     public Car()
-     {
-         CarLot.cnt++
-     }
+    class Car
+    {
+        public int year { get; set; } = default(int);
+        public string model { get; set; } =default(string);
+        public string make { get; set; } = default(string);
+        public bool IsDriveable { get; set; } =default(bool);
+        public string HonkNoise { get; set; } = default(string);
+        public string EngineNoise { get; set; } = default(string);
+        public Car()
+        {
+            CarLot.cnt++;
+        }
 
-     public Car(int year,string model,string make,bool IsDriveable,string HonkNoise,string EngineNoise)
-     {
-     CarLot.cnt++
-     this.year=year;
-     this.model=model;
-     this.make=make;
-     this.IsDriveable=IsDriveable;
-     this.HonkNoise=HonkNoise;
-     this.EngineNoise=EngineNoise;
-     }
+        public Car(int year, string model, string make, bool IsDriveable, string HonkNoise, string EngineNoise)
+        {
+            CarLot.cnt++;
+        this.year = year;
+            this.model = model;
+            this.make = make;
+            this.IsDriveable = IsDriveable;
+            this.HonkNoise = HonkNoise;
+            this.EngineNoise = EngineNoise;
+        }
 
-public void MakeEngineNoise(string n)
-{
-Console.WriteLine(n);
-}
-
-
-public void MakeHonkNoise()
-{
-Console.WriteLine(HonkNoise);;
-}
+        public void MakeEngineNoise(string n)
+        {
+            Console.WriteLine(n);
+        }
 
 
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
 
 
-}
+
+
+    }
 }
