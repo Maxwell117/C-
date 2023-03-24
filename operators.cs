c826a5d4dc256ec23f2430527dfbb4f103de858d
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 00:47:17 -0400

    Update operators.cs

--- operators.cs
+++ operators.cs
@@ -1,27 +1,27 @@
 using system;
 using math;
-namespace MethodExercise
+namespace OperatorExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+     static void Main(string[] args)
         {
-           var a=27;
-           var b=4;
-           var quot=a/b;
-           var rem=a%b;
-            double rad=77;
-           double area;
-           console.writeline(a+"/ "+b+"= "+ quot);
-           console.writeline(a+"% "+b+"= "+ rem);
-        area=  areaofacircle(rad);
-          
+            var a = 27;
+            var b = 4;
+            var quot = a / b;
+            var rem = a % b;
+            double rad = 77;
+            double area;
+            Console.WriteLine(a + "/ " + b + "= " + quot);
+            Console.WriteLine(a + "% " + b + "= " + rem);
+            area = areaofacircle(rad);
+
 
         }
 
-         public static double areaofacircle(double radios)
-           {
-           return Math.PI * Math.Pow(radius, 2);
-           }
+        public static double areaofacircle(double radios)
+        {
+            return Math.PI * Math.Pow(radios, 2);
+        }
     }
 }
