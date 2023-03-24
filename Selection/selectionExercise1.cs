13384ce2b2f6cd2017c18d5727c4b9622ccfd09c
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 00:55:26 -0400

    Add files via upload

---
+++ Selection/selectionExercise1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodExercise
+{
+	public class Program
+	{
+		static void main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			var slotmachine = new Random();
+			Console.WriteLine("Please enter a number");
+			int rand = slotmachine.Next(1, 100);
+			if (rand > n)
+			{
+				Console.WriteLine("Too high");
+			}
+			else if (rand < n)
+			{
+				Console.WriteLine("Too low");
+			}
+			else
+			{
+				Console.WriteLine("You found it!");
+			}
+		}
+	}
+}
---
+++ Selection/selectionExercise2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodExercise
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What is your favorite subject in school?");
+			var sub = Console.ReadLine();
+
+			switch (sub)
+			{
+				case "math":
+					Console.WriteLine("torture");
+					break;
+				case "english":
+					Console.WriteLine("drama");
+					break;
+				case "chemistry":
+					Console.WriteLine("breaking bad");
+					break;
+				case "art":
+					Console.WriteLine("fun");
+					break;
+				case "history":
+					Console.WriteLine("boring");
+					break;
+				default:
+					Console.WriteLine("Never heard of it");
+					break;
+			}
+		}
+	}
+}
