fbef2bf7e4d448944f7007195c7dc906e2b94ac5
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 02:10:35 -0400

    Create CarMain.cs

---
+++ static/CarMain.cs
@@ -0,0 +1,19 @@
+using System;
+namespace car
+{
+    public class Program
+    {
+        static void main(string[] args)
+        {
+
+            Car honda = new Car();
+            Console.WriteLine(CarLot.cnt);
+            Car chey = new Car();
+            Console.WriteLine(CarLot.cnt);
+            Car troa = new Car();
+            Console.WriteLine(CarLot.cnt);
+
+        }
+
+    }
+}
