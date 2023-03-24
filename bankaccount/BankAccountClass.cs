603402e22493852cc38197a0352c72196fd71b3c
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-24 01:50:43 -0400

    Update BankAccountClass.cs

--- bankaccount/BankAccountClass.cs
+++ bankaccount/BankAccountClass.cs
@@ -2,27 +2,30 @@ using System;
 using System.Collections.Generic;
 namespace bankaccount
 {
-class bankaccount 
-{
- 
- private double balance=0;
+    class bankaccount
+    {
 
-     public bankaccount()
-     {
+        private double balance = 0;
 
-     }
+        public bankaccount()
+        {
 
-     public double Balance()
-     {
-      return balance;
-     }
+        }
 
-     public void Deposit(double amount)
-     {
-     balance=amount;
-     }
+        public double GetBalance()
+        {
+            return balance;
+        }
 
+        public void SetBalance(double b)
+        {
+            balance = b;
+        }
 
-
-}
+  
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+    }
 }
